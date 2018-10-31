7367220e552b5f83492a56b3c136147c1b7ba41d
sumitskj <sumitjangirdss.1@gmail.com>
2018-10-31 10:07:06 +0530

    final commit added casting vote and events

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -7,11 +7,17 @@ contract Election {
         string name;
         uint voteCount;
     }
+    //recording voted or not
+    mapping(address => bool) public voters;
     //fetch the data
     mapping(uint => Candidate) public candidates;   
     // candidates count
-    uint public candidateCount;
+    uint public candidatesCount;
 
+    //events
+    event votedEvent (
+        uint indexed _candidateId
+    );
     //Constructor
     function Election() public {
         addCandidate("Candidate 1");
@@ -20,9 +26,24 @@ contract Election {
 
     //Add Candidates
     function addCandidate(string _name) private {
-        candidateCount++;
-        candidates[candidateCount]= Candidate(candidateCount, _name, 0);
+        candidatesCount++;
+        candidates[candidatesCount]= Candidate(candidatesCount, _name, 0);
     }
 
+    function vote(uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        votedEvent(_candidateId);
+    }
 
 }
